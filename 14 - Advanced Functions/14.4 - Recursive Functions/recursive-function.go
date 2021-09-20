a1856bbb1162a7039499c378b599c93b84b20c94
Fabiano Yoschitaki <fabiano.yoschitaki@bairesdev.com>
2021-09-20 20:34:30 -0300

    Functions

--- 14 - Advanced Functions/14.4 - Recursive Functions/recursive-function.go
+++ 14 - Advanced Functions/14.4 - Recursive Functions/recursive-function.go
@@ -1,16 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-// 1 1 2 3 5 8 13 21 34
-func fibonacci(n int) int {
+// 0 1 1 2 3 5 8 13 21 34
+func fibonacci(n uint) uint {
 	if n <= 1 {
-		return 1
+		return n
 	}
 	return fibonacci(n-1) + fibonacci(n-2)
 }
 
 func main() {
-	number := 10
+	number := uint(10)
 	fmt.Println(fmt.Sprintf("Fibonacci of %d is: %d", number, fibonacci(number)))
+
+	// printing all fibo
+	for i := uint(0); i <= number; i++ {
+		fmt.Println(fmt.Sprintf("Fibo of position %d is %d", i, fibonacci(i)))
+	}
 }
